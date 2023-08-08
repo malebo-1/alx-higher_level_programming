ad64c568bfd5928e9630d6c43ae4b4e5e2ca3e09
malebo komape <malebokomape788@gmail.com>
2023-08-08 13:47:17 -0700

    0x01. Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#4-print_hexa.py
+
+"printing numbers from 0 to 98 in decimal & hexadecimal"
+for number in range(0,99):
+    print("{} = {}".format(number,hex(number)))
---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,13 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+def islower(c);
+def uppercase(str);
+listint_t *insert_node(listint_t **head, int number);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+#endif /* LISTS_H */
