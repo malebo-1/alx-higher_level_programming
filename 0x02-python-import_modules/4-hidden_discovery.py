8779d5d7dd21953236d5fcbedeb3cbd426336dc7
malebo komape <malebokomape788@gmail.com>
2023-08-11 13:03:39 -0700

    prints all the names defined by the compiled module hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__"
+    import hidden_4
+    # sorted name to be printed from the directory
+    for name in sorted(dir(hidden_4)):
+        # only names that do not start with __ must be printed
+        if name[:2] != '__':
+            print("{}".format(name))
