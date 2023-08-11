8e761f2bec06c0e8f5ecca0fda7516b5d53ab3e4
malebo komape <malebokomape788@gmail.com>
2023-08-11 13:02:47 -0700

    prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_arg(argv):
+    j = len(argv) - 1
+        if j == 0:
+            print("{:d}".format(j))
+            return
+        else:
+            k = 1
+            add = 0
+            while k <= j:
+                add += int(argv[k])
+                k += 1
+            print("{:d}".format(add))
+
+if __name__ == "__main__":
+    import sys
+    add_arg(sys.argv)
