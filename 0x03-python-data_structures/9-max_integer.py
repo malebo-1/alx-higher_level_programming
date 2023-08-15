0d82652d2accb3ec0ed23d33de1fcf34e103759c
malebo komape <malebokomape788@gmail.com>
2023-08-15 05:09:02 -0700

    a function that finds the biggest integer of a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 9-max_integer.py
+
+def max_integer(my_list=[]):
+    """Finds the biggest integer of list"""
+    if len(my_list) == 0:
+        return (None)
+    
+    big = my_list[0]
+    for n in range(len(my_list)):
+        if my_list[n] > big:
+            big = my_list[n]
+
+        return (big)
